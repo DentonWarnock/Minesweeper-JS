b30b4aebdce675ef2a96294bb6e8ff9b997ef3a4
DentonWarnock <dentonwarnock@gmail.com>
2020-08-01 15:03:55 -0700

    random gameboard with 20 bombs now generates on page load/refresh.

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let width = 10;
+  let bombAmount = 20;
   let squares = [];
 
   // create game board
   function createBoard() {
+    // get shuffled game array with random bombs
+    const bombsArray = Array(bombAmount).fill("bomb");
+    const emptyArray = Array(width * width - bombAmount).fill("valid");
+    // join the above 2 arrays together
+    const gameArray = emptyArray.concat(bombsArray);
+    // generate random game board
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement("div");
       square.setAttribute("id", i);
+      square.classList.add(shuffledArray[i]);
       grid.appendChild(square);
       squares.push(square);
     }
